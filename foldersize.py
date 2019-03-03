995ab46a66d3e3b17b52840d65c1dbbebe796fb9
Paul Wilson <pwilson802@gmail.com>
2019-03-03 22:17:41 +1100

    foldersize added

---
+++ foldersize.py
@@ -0,0 +1,19 @@
+# Folder size finds the size of the folder given to the script
+import sys
+import os
+myfolder = sys.argv[1]
+
+totalsize = 0
+
+for folder, x, files in os.walk(myfolder):
+    foldersize = 0
+    for file in files:
+        try:
+            totalsize = totalsize + os.path.getsize(os.path.join(folder, file))
+        except FileNotFoundError:
+            print('File not Found')
+#    try:
+#        totalsize = totalsize + os.path.getsize(os.path.join(myfolder, file))
+#    except FileNotFoundError:
+#        print('File not Found')
+print(totalsize / 1000 / 1000)
