5795d10f7f4fc96524af26fe51d725a6b8c555d9
Paul Wilson <pwilson802@gmail.com>
2019-03-04 11:28:31 +1100

    added argparse

--- foldersize.py
+++ foldersize.py
@@ -1,23 +1,29 @@
 # Folder size finds the size of the folder given to the script
 import sys
+import argparse
 import os
-myfolder = sys.argv[1]
+parser = argparse.ArgumentParser(description='Find the size of folders')
+parser.add_argument('foldername', help='The top folder to check the size of')
+parser.add_argument('--topfolders', '-t', type=int, help='The top number of folders to show')
+parser.add_argument('--version', action='version', version='%(prog)s 2.0')
+
+args = parser.parse_args()
+# myfolder = sys.argv[1]
 
 totalsize = 0
-allfolders = {}
+allfolders = []
 
-for folder, x, files in os.walk(myfolder):
+for folder, x, files in os.walk(foldername):
     foldersize = 0
     for file in files:
         try:
-            totalsize = totalsize + os.path.getsize(os.path.join(folder, file))
-        except FileNotFoundError:
-            print('File not Found')
-    allfolders[folder] = foldersize
+            foldersize = foldersize + os.path.getsize(os.path.join(folder, file))
+        except FileNotFoundError as err:
+            continue
+    allfolders.append([folder, foldersize])
+    totalsize += foldersize
+allfolders.sort(key=lambda k: k[1], reverse=True)
 
-#    try:
-#        totalsize = totalsize + os.path.getsize(os.path.join(myfolder, file))
-#    except FileNotFoundError:
-#        print('File not Found')
 print(totalsize / 1000 / 1000)
-print(allfolders)
+for x in allfolders[:5]:
+    print('{0}: {1:.2f}'.format(x[0], x[1] / 1000 / 1000))
